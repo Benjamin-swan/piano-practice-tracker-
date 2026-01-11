a29f75216c68c7bf5dc4f52e0ae2ccbf60fcd298
Benjamin-swan <jeon221b@gmail.com>
2026-01-11 22:56:01 +0900

    delete. [benjamin] 로그인 관련 코그 삭제 [ppt-6]

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -12,10 +11,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
